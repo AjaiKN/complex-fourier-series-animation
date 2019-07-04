45bd1f3d1f05ac4c5d646c8f860c8f0c6c665063
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2019-07-04 16:43:45 -0500

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,408 @@
+module Main exposing (Model, Msg(..), average, backAndForthTermNum, constantN, coordTransform, distTransform, init, main, makeCircle, makeLine, myRange, squareIm, squareRe, subscriptions, sumToTerm, term, theFunction, update, view)
+
+import Array exposing (Array)
+import Browser
+import Complex exposing (..)
+import Dict exposing (Dict)
+import Html exposing (Html, div, input, label, span, text)
+import Html.Attributes exposing (class, step, type_, value)
+import Html.Events exposing (onInput)
+import Svg exposing (Svg, circle, line, rect, svg)
+import Svg.Attributes exposing (cx, cy, fill, height, r, stroke, width, x, x1, x2, y, y1, y2)
+import Time
+
+
+squareRe : Float -> Float
+squareRe t =
+    if t <= 0.25 then
+        1
+
+    else if t <= 0.5 then
+        3 - 8 * t
+
+    else if t <= 0.75 then
+        -1
+
+    else
+        -7 + 8 * t
+
+
+squareIm : Float -> Float
+squareIm t =
+    if t <= 0.25 then
+        -1 + 8 * t
+
+    else if t <= 0.5 then
+        1
+
+    else if t <= 0.75 then
+        5 - 8 * t
+
+    else
+        -1
+
+
+squareFunction : Float -> Complex
+squareFunction t =
+    complex (squareRe t) (squareIm t)
+
+
+cosFunction : Float -> Complex
+cosFunction =
+    turns >> cos >> real
+
+
+stepFunction : Float -> Complex
+stepFunction t =
+    if t < 0.5 then
+        real 1
+
+    else
+        real -1
+
+
+cosFunction2D : Float -> Complex
+cosFunction2D t =
+    complex
+        (t * 4 - 2)
+        (t * 3 |> turns |> sin)
+
+
+theFunction : Float -> Complex
+theFunction t1 =
+    let
+        t =
+            t1 - toFloat (floor t1)
+    in
+    cosFunction2D t
+
+
+myRange : Array Float
+myRange =
+    Array.fromList <| List.map (toFloat >> (*) 0.001) (List.range 0 999)
+
+
+average : Array Complex -> Complex
+average arr =
+    divide (Array.foldl add zero arr) (Array.length arr |> toFloat |> real)
+
+
+constantN : (Float -> Complex) -> Int -> Complex
+constantN f n =
+    average <|
+        Array.map
+            (\t -> multiply (f t) (exp (imaginary (-2.0 * pi * toFloat n * t))))
+            myRange
+
+
+memoize : (comparable -> b) -> List comparable -> comparable -> b
+memoize f vals =
+    let
+        dict : Dict comparable b
+        dict =
+            Dict.fromList <|
+                List.map (\n -> ( n, f n )) vals
+    in
+    \inp ->
+        case Dict.get inp dict of
+            Just n ->
+                n
+
+            Nothing ->
+                f inp
+
+
+constantNMemoized : Int -> Complex
+constantNMemoized =
+    memoize (constantN theFunction) (List.range -30 30)
+
+
+term : (Float -> Complex) -> Int -> Float -> Complex
+term f n t =
+    multiply (constantNMemoized n) (exp (imaginary (2.0 * pi * toFloat n * t)))
+
+
+backAndForthTermNum : Int -> Int
+backAndForthTermNum n =
+    --0,1,-1,2,-2,3,-3,...
+    if modBy 2 n == 0 then
+        n // 2
+
+    else
+        -(n // 2 + 1)
+
+
+sumToTerm : (Float -> Complex) -> Int -> Float -> Complex
+sumToTerm f n t =
+    List.foldl
+        (\newN prevSum ->
+            add prevSum <|
+                term f (backAndForthTermNum newN) t
+        )
+        zero
+        (List.range 0 n)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { originalTime : Int
+    , sinceStart : Int
+    , speed : String
+    , numVectors : String
+    , zoom : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model -100 0 (floatToStr 8) "40" "2", Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = InitialTime Time.Posix
+    | Tick Time.Posix
+    | Speed String
+    | NumVectors String
+    | Zoom String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        InitialTime t ->
+            ( { model | originalTime = Time.posixToMillis t, sinceStart = 0 }
+            , Cmd.none
+            )
+
+        Tick t ->
+            ( { model | sinceStart = Time.posixToMillis t - model.originalTime }
+            , Cmd.none
+            )
+
+        Speed s ->
+            ( { model | speed = s }
+            , Cmd.none
+            )
+
+        NumVectors s ->
+            ( { model | numVectors = s }
+            , Cmd.none
+            )
+
+        Zoom s ->
+            ( { model | zoom = s }
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Time.every 10 Tick
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ viewInputs model, viewAnimation model ]
+
+
+viewInputs : Model -> Html Msg
+viewInputs model =
+    div []
+        [ numInput Speed model.speed "Speed (cycles per minute)" "any"
+        , numInput NumVectors model.numVectors "Number of spinning vectors" "1"
+        , numInput Zoom model.zoom "Zoom" "any"
+        ]
+
+
+viewAnimation : Model -> Html Msg
+viewAnimation ({ sinceStart } as model) =
+    let
+        time =
+            toFloat sinceStart / 1000 / 60 * speed
+
+        { speed, numVectors, zoom } =
+            getOptions model
+
+        final =
+            numVectors - 1
+    in
+    svg [ width "900", height "900" ] <|
+        rect
+            [ x (coordTransform zoom -1)
+            , y (coordTransform zoom -1)
+            , width (distTransform zoom 2)
+            , height (distTransform zoom 2)
+            , stroke "green"
+            , fill "none"
+            ]
+            []
+            :: List.concatMap
+                (\n ->
+                    let
+                        current =
+                            sumToTerm theFunction n time
+
+                        distanceToNext =
+                            (Complex.toPolar (term theFunction (backAndForthTermNum (n + 1)) time)).abs
+                    in
+                    if distanceToNext < 0.0001 then
+                        []
+
+                    else
+                        [ makeLine
+                            current
+                            (sumToTerm theFunction (n + 1) time)
+                            zoom
+                        , makeCircle current distanceToNext "red" "none" zoom
+                        , makeCircle current 0.015 "none" "blue" zoom
+                        ]
+                )
+                (List.range 0 final)
+            ++ [ makeCircle (sumToTerm theFunction (final + 1) time) 0.03 "none" "green" zoom ]
+
+
+makeCircle : Complex -> Float -> String -> String -> Float -> Svg Msg
+makeCircle a radius color f zoom =
+    let
+        c =
+            toCartesian a
+    in
+    circle
+        [ cx (coordTransform zoom c.re)
+        , cy (coordTransform zoom c.im)
+        , r (distTransform zoom radius)
+        , stroke color
+        , fill f
+        ]
+        []
+
+
+makeLine : Complex -> Complex -> Float -> Svg Msg
+makeLine a1 a2 zoom =
+    let
+        c1 =
+            toCartesian a1
+
+        c2 =
+            toCartesian a2
+    in
+    line
+        [ x1 (coordTransform zoom c1.re)
+        , y1 (coordTransform zoom c1.im)
+        , x2 (coordTransform zoom c2.re)
+        , y2 (coordTransform zoom c2.im)
+        , stroke "red"
+        ]
+        []
+
+
+coordTransform : Float -> Float -> String
+coordTransform zoom f =
+    String.fromFloat ((f + zoom) * 100 / zoom / 2) ++ "%"
+
+
+distTransform : Float -> Float -> String
+distTransform zoom f =
+    String.fromFloat (f * 100 / zoom / 2) ++ "%"
+
+
+numInput : (String -> Msg) -> String -> String -> String -> Html Msg
+numInput changer val lab step_ =
+    Html.span []
+        [ label [] [ text lab ]
+        , divClass "input-group"
+            [ input
+                [ class "numberInput form-control"
+                , onInput changer
+                , type_ "number"
+                , value val
+                , step step_
+                ]
+                []
+            ]
+        ]
+
+
+divClass c =
+    div [ class c ]
+
+
+divClass1 c thing =
+    div [ class c ] [ thing ]
+
+
+
+--Float and String
+
+
+strToFloat : String -> Float
+strToFloat s =
+    case String.toFloat s of
+        Just f ->
+            f
+
+        Nothing ->
+            0
+
+
+strToInt : String -> Int
+strToInt s =
+    case String.toInt s of
+        Just f ->
+            f
+
+        Nothing ->
+            0
+
+
+floatToStr : Float -> String
+floatToStr =
+    String.fromFloat
+
+
+type alias Options =
+    { speed : Float
+    , numVectors : Int
+    , zoom : Float
+    }
+
+
+getOptions : Model -> Options
+getOptions { speed, numVectors, zoom } =
+    { speed = strToFloat speed
+    , numVectors =
+        if strToInt numVectors < 80 && strToInt numVectors >= 0 then
+            strToInt numVectors
+
+        else
+            0
+    , zoom = strToFloat zoom
+    }
